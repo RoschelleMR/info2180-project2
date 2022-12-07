6e668ab7d65c92fc06ac6d38e11293a039c960d3
NiaPayton <shadeanpayton@gmail.com>
2022-12-06 23:52:10 -0500

    added created new contact

---
+++ server.php
@@ -0,0 +1,9 @@
+<?php 
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'dolphin_crm';
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
